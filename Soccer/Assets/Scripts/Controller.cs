898a0248dc55057e7708a711e7337d7285580f93
Hoàng <44893025+TheHoang3112@users.noreply.github.com>
2020-07-29 16:32:49 +0700

    add file

---
+++ Soccer/Assets/Scripts/Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    public void Question_1()
+    {
+      
+        SceneManager.LoadScene(1);
+    }
+    public void Question_2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Question_3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Question_4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Question_5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Question_6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Question_7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void Question_8()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void Question_9()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void Question_10()
+    {
+        SceneManager.LoadScene(10);
+    }
+}
---
+++ Soccer/Assets/Scripts/Question.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Question : MonoBehaviour
+{
+    public GameObject Question_1;
+    public GameObject Answers;
+    public Button Home_Question;
+    public Button Button_Back;
+
+    public void Click_Button_QS1()
+    {
+        Question_1.SetActive(false);
+        Answers.SetActive(true);
+        Home_Question.gameObject.SetActive(true);
+        Button_Back.gameObject.SetActive(false);
+       // Answers.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+    }
+
+    public void Home_QS()
+    {
+        Question_1.SetActive(true);
+        Answers.SetActive(false);
+        Home_Question.gameObject.SetActive(false);
+        Button_Back.gameObject.SetActive(true);
+    }
+
+    public void BT_Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
